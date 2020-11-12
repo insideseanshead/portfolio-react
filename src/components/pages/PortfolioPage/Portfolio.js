9ae6a858d349bfbee1988c5349c1822deeeba996
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-11-11 19:10:06 -0800

    fixed portfolio page

--- src/components/pages/PortfolioPage/Portfolio.js
+++ src/components/pages/PortfolioPage/Portfolio.js
@@ -1,17 +1,40 @@
 import React from "react";
 import "./style.css"
+import freshest from "../../../assets/freshest.png"
+import dnd from "../../../assets/dnd.png"
+import weather from "../../../assets/weather_dash.png"
+import passgen from "../../../assets/pass_gen.png"
 
 function Portfolio() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+     <div class="container">
+        <div class="row" id="rowTwo">
+            <div class="col-md-12" id="heading">
+                <h1 id="port">Portfolio: GitHub Projects</h1>
+            </div>
+            <div class="col-md-4 .col-sm-6">
+                <h5><a href="https://clubkemp.github.io/freshair/">Fresh Air Application</a></h5>
+                <img src={freshest} id="pic-one" alt="comet" />
+                <p>GitHub:<a href="https://github.com/clubkemp/freshair">Fresh Air Repository Link</a></p>
+            </div>
+            <div class="col-md-4">
+              <h5><a href="https://stark-island-65376.herokuapp.com/">Fantasy Character Manager Application</a></h5>
+                <img src={dnd} id="pic-two" alt="deployed app link" />
+                <p>GitHub:<a href="https://github.com/artuis/dnd-action-calculator">Fantasy Character Builder</a></p>
+            </div>
+    </div>
+    <div class="row" id="rowThree">
+        <div class="col-md-4 .col-sm-6">
+          <h5><a href="https://magnus-jay.github.io/weather-dashboard/">Weather Dashboard Application</a></h5>
+            <img src={weather} id="pic-three"  alt="moon" />
+            <p>GitHub:<a href="https://github.com/Magnus-Jay/weather-dashboard">Weather Dashboard</a></p>
+        </div>
+        <div class="col-md-4 .col-sm-6">
+          <h5><a href="https://magnus-jay.github.io/password--gen/">Password Generator Application</a></h5>
+            <img src={passgen} id="pic-four" alt="stars and trees" />
+            <p>GitHub:<a href="https://github.com/Magnus-Jay/password--gen">Password Generator</a></p>
+        </div>
+    </div>
     </div>
   );
 }
