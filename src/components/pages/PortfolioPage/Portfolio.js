8f34a8559323ef07016327d0fd97fb1729ad7639
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-11-11 21:28:40 -0800

    added two more projects

--- src/components/pages/PortfolioPage/Portfolio.js
+++ src/components/pages/PortfolioPage/Portfolio.js
@@ -4,6 +4,8 @@ import freshest from "../../../assets/freshest.png"
 import dnd from "../../../assets/dnd.png"
 import weather from "../../../assets/weather_dash.png"
 import passgen from "../../../assets/pass_gen.png"
+import Note from "../../../assets/note-taker.png"
+import Todo from "../../../assets/todo.png"
 
 function Portfolio() {
   return (
@@ -18,9 +20,14 @@ function Portfolio() {
                 <p>GitHub:<a href="https://github.com/clubkemp/freshair" className="port-link">Fresh Air Repository Link</a></p>
             </div>
             <div class="col-md-4">
-              <h5><a href="https://stark-island-65376.herokuapp.com/">Fantasy Character Manager Application</a></h5>
+              <h5><a href="https://stark-island-65376.herokuapp.com/">Fantasy Character Application</a></h5>
                 <img src={dnd} id="pic-two" alt="deployed app link" />
                 <p>GitHub:<a href="https://github.com/artuis/dnd-action-calculator" className="port-link">Fantasy Character Builder Link</a></p>
+            </div>
+            <div class="col-md-4">
+              <h5><a href="https://calm-wildwood-27402.herokuapp.com/">Note Taker Application</a></h5>
+                <img src={Note} id="pic-three" alt="deployed app link" />
+                <p>GitHub:<a href="https://github.com/Magnus-Jay/note-taker" className="port-link">Note Taker Link</a></p>
             </div>
     </div>
     <div class="row" id="rowThree">
@@ -34,8 +41,13 @@ function Portfolio() {
             <img src={passgen} id="pic-four" alt="stars and trees" />
             <p>GitHub:<a href="https://github.com/Magnus-Jay/password--gen" className="port-link">Password Generator Link</a></p>
         </div>
+        <div class="col-md-2 .col-sm-6">
+          <h5><a href="https://magnus-jay.github.io/to-do-list/">To-do Application</a></h5>
+            <img src={Todo} id="pic-six"  alt="moon" />
+            <p>GitHub:<a href="https://github.com/Magnus-Jay/to-do-list" className="port-link">To-do-list Link</a></p>
+        </div>  
     </div>
-    </div>
+  </div>
   );
 }
 
