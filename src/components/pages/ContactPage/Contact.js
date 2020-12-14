0a51de25973402af7c0653ef676eafe6fda8985d
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-12-14 11:28:02 -0800

    added email to bio

--- src/components/pages/ContactPage/Contact.js
+++ src/components/pages/ContactPage/Contact.js
@@ -8,8 +8,6 @@ function Contact(props) {
     <div className="container">
         <div className="row">
           <div className="col-md-12" id="heading">
-            <h1 id="contact-heading">Contact
-            </h1>
             <hr />
           </div>
           <div className="col-md-6">
@@ -30,10 +28,12 @@ function Contact(props) {
             </p>
           </div>
           <div className="col-md-6 contact-form">
+          <h1>Contact</h1>
             <p>
               Thank you for visiting my portfolio and I hope you enjoyed what you have seen so far. Please feel free to send me an email using the contact-form to the left. Additionally there is a link to my LinkedIn page where you can find additional information about myself and experience. 
             </p>
-            <br />
+            {/* <br /> */}
+            <p>Email: <a href="mailto:magnus86@uw.edu">magnus86@uw.edu</a></p>
             <p>Check out my work history and my professional profile on:</p>
             <button className="btn btn-dark" id="linked"><a href="https://www.linkedin.com/in/magnusappe/">LinkedIn Profile</a></button>
             <br />
